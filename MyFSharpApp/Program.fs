9f1fe57f5a7870fb5b600fcc7f7c7ebf3d13c1ba
soojeensin <soojeensin@gmail.com>
2022-06-08 10:20:06 +1000

    f# study

---
+++ MyFSharpApp/Program.fs
@@ -0,0 +1,10 @@
+﻿// Define a new function to print a name.
+let printGreeting name =
+    printfn $"Hello {name} from F#!"
+
+// Call your new function!
+printGreeting "Ailen"
+
+
+// For more information see https://aka.ms/fsharp-console-apps
+// printfn "Hello from F#"
