32343730c259e63eeb0228d55ca113703b8239ab
soojeensin <soojeensin@gmail.com>
2022-06-09 11:31:57 +1000

    Variable Types

--- MyFSharpApp/Program.fs
+++ MyFSharpApp/Program.fs
@@ -1,4 +1,29 @@
-﻿// Define a new function to print a name.
+﻿
+// Type
+let age = 65 // int
+let PI = 3.14 // float
+let name1 = "my name" // string
+
+let sum:float = 0.0
+
+// String interpolation
+let name = "Luis"
+let company = "Microsoft"
+printfn $"Name: {name}, Company: {company}"
+
+let firstNumber = 2000
+let secondNumber = 21
+printfn $"The year is: {firstNumber + secondNumber}"
+
+let name3 = "Chris"
+printf "Hi %s" name3
+// prints: Hi Chris
+
+// %s string 
+// %d, %i decimal 
+// %b true false 
+
+// Define a new function to print a name.
 let printGreeting name =
     printfn $"Hello {name} from F#!"
 
