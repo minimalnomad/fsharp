b1635c0568694c8634364e9e904c5f5d6dc51c91
soojeensin <soojeensin@gmail.com>
2022-07-07 11:52:45 +1000

    refactor update_tax_dom

--- update_tax_dom.js
+++ update_tax_dom.js
@@ -1,3 +1,7 @@
 function update_tax_dom(){
-    set_tax_dom(shopping_cart_total * 0.10);
+    set_tax_dom(calc_tax(shopping_cart_total));
+}
+
+function calc_tax(amount){
+    return amount * 0.10;
 }
